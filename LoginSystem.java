3da433d66bdc5701415dc5fb36378aaac3905ffa
Sneha Sharma <snehassharma3003@gmail.com>
2026-03-22 16:28:01 +0530

    Create LoginSystem.java

---
+++ LoginSystem.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LoginSystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String username = "admin";
+        String password = "1234";
+
+        System.out.print("Enter username: ");
+        String u = sc.nextLine();
+
+        System.out.print("Enter password: ");
+        String p = sc.nextLine();
+
+        if (u.equals(username) && p.equals(password)) {
+            System.out.println("Login Successful!");
+        } else {
+            System.out.println("Invalid credentials!");
+        }
+
+        sc.close();
+    }
+}
